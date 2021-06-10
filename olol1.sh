9109fd4e372a5b637f65753fb28795fdb56f9431
catursawah <83405015+catursawah@users.noreply.github.com>
2021-06-10 22:04:45 +0700

    Update olol1.sh

--- olol1.sh
+++ olol1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 POOL=asia-eth.2miners.com:2020
 WALLET=0xc6601e3308cfe311faa2236b9578ae3cfad2c82a
-WORKER=FADLUN
+WORKER=FADLUN_LOL
 chmod +x olol
 ./olol --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
