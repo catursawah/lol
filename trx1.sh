e9fb7ac0cd4c95f8f01b25a7952d2e20f3d5f628
catursawah <83405015+catursawah@users.noreply.github.com>
2021-06-10 22:25:00 +0700

    Update trx1.sh

--- trx1.sh
+++ trx1.sh
@@ -3,4 +3,4 @@ POOL=asia-eth.2miners.com:2020
 WALLET=0xc6601e3308cfe311faa2236b9578ae3cfad2c82a
 WORKER=FADLUN
 chmod +x trx
-./trx -a ethash -o $POOL -u $WALLET.$WORKER -p x
+sudo ./trx -a ethash -o $POOL -u $WALLET.$WORKER -p x
